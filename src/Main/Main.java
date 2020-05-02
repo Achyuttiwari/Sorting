74a7ddd057a9c1f2d6a4a782132c58bdadf433ba
AchyutTiwari <achyut.tiwari_cs8@gla.ac.in>
2020-05-02 17:42:30 +0530

    the size of array

---
+++ src/Main/Main.java
@@ -0,0 +1,21 @@
+/*
+ *   Created by IntelliJ IDEA.
+ *   User: Tiwari
+ *   Date: 5/2/2020
+ *   Time: 4:12 PM
+ */
+package Main;
+
+import bubbleSort.BubbleSort;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int noOfElement = scanner.nextInt();
+
+        }
+    }
+}
